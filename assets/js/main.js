659a06996d88b58463e823e82dc3367f6407e9af
sumancfc <sumanstha999@gmail.com>
2020-11-11 19:01:57 +0545

    Hero section added on home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -216,4 +216,31 @@
       });
   }
   sidemenuDropdown();
+
+  // Slider active
+  $(".slider__area-active").owlCarousel({
+    loop: true,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: 5000,
+    navText: [
+      '<i class="la la-angle-up"></i>',
+      '<i class="la la-angle-down"></i>',
+    ],
+    animateOut: "fadeOut",
+    animateIn: "fadeIn",
+    dotsData: true,
+    item: 1,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 1,
+      },
+      1000: {
+        items: 1,
+      },
+    },
+  });
 })(jQuery);
