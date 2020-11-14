fed6ce4e87ab844f3c6703be6a9b603e85ddd155
sumancfc <sumanstha999@gmail.com>
2020-11-14 11:01:28 +0545

    Product modal added on product

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,4 +272,57 @@
       },
     },
   });
+
+  // Modal view slider
+  $(".quickview__slide").owlCarousel({
+    loop: true,
+    navText: [
+      "<i class='la la-angle-left'></i>",
+      "<i class='la la-angle-right'></i>",
+    ],
+    margin: 15,
+    smartSpeed: 1000,
+    nav: true,
+    dots: false,
+    responsive: {
+      0: {
+        items: 3,
+        autoplay: true,
+        smartSpeed: 300,
+      },
+      576: {
+        items: 3,
+      },
+      768: {
+        items: 3,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  $(".quickview__slide a").on("click", function () {
+    $(".quickview__slide a").removeClass("active");
+  });
+
+  // Cart + -
+  var CartPlusMinus = $(".cart__plus-minus");
+  CartPlusMinus.prepend('<div class="dec qtybutton">-</div>');
+  CartPlusMinus.append('<div class="inc qtybutton">+</div>');
+  $(".qtybutton").on("click", function () {
+    var $button = $(this);
+    var oldValue = $button.parent().find("input").val();
+    if ($button.text() === "+") {
+      var newVal = parseFloat(oldValue) + 1;
+    } else {
+      // Don't allow decrementing below zero
+      if (oldValue > 0) {
+        var newVal = parseFloat(oldValue) - 1;
+      } else {
+        newVal = 1;
+      }
+    }
+    $button.parent().find("input").val(newVal);
+  });
 })(jQuery);
